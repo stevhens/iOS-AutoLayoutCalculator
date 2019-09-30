22b7f8a1bb924f1414119a6b92932dc109b3ad39
Steven Sim <stevensim@Stevensim.local>
2019-09-30 17:35:19 +0700

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Steven Sim on 13/08/19.
+//  Copyright © 2019 Steven Sim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
